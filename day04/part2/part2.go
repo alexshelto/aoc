fbb4e1e6c5232c1beb674ed44850d832281a93e7
alexshelto <alexandershelton@icloud.com>
2022-12-04 00:35:09 -0500

    day04

---
+++ day04/part2/part2.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+
+type Assignment struct {
+    low int 
+    high int 
+}
+
+func NewAssignment(r string) Assignment {
+    section_range := strings.Split(r, "-")
+    low, err := strconv.Atoi(section_range[0])
+    if err != nil {
+        panic(err)
+    }
+    high, err := strconv.Atoi(section_range[1])
+    if err != nil {
+        panic(err)
+    }
+    return Assignment{low: low, high: high}
+}
+
+
+func RangeEncapsulates(a1 *Assignment, a2 *Assignment) bool {
+    if a2.low >= a1.low && a2.low <= a1.high || 
+        a1.low >= a2.low && a1.low <= a2.high {
+            return true
+        }
+    return false
+}
+
+
+func compute(input string) int {
+    scanner := bufio.NewScanner(strings.NewReader(input))
+    ret := 0
+
+    for scanner.Scan() {
+        ranges := strings.Split(scanner.Text(), ",")
+        if len(ranges) != 2 {
+            panic("invalid input")
+        }
+
+        A1 := NewAssignment(ranges[0])
+        A2 := NewAssignment(ranges[1])
+
+        if RangeEncapsulates(&A1, &A2) {
+            ret += 1
+        }
+    }
+    return ret
+}
+
+func main() {
+    bytes, err := os.ReadFile("../input.txt")
+    if err != nil {
+        panic(err)
+    }
+    str := string(bytes)
+    fmt.Println(compute(str))
+}
---
+++ day04/part2/part2_test.go
@@ -0,0 +1,25 @@
+package main 
+
+import "testing"
+
+var testParams = []struct {
+    in string 
+    out int 
+} {
+    {`2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`, 4},
+}
+
+
+func TestSolution(t *testing.T) {
+    for _,tp := range testParams {
+        got := compute(tp.in)
+        if got != tp.out {
+            t.Errorf("got: %v, wanted: %v", got, tp.out)
+        }
+    }
+}
