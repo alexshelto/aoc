c35c94c637852ac135d3644f8ca112517fbfafe6
alexshelto <alexandershelton@icloud.com>
2022-12-07 21:54:52 -0500

    welp there goes my wednesday

--- day07/part1/part1.go
+++ day07/part1/part1.go
@@ -3,122 +3,82 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
+	"path/filepath"
 	"strings"
+	"strconv"
 )
 
 var MAX_DIR_SIZE = 100000
 
-type PathStack struct {
-    paths []string 
+
+
+type Set map[string]bool
+
+func (s *Set) Add(val string) {
+    (*s)[val] = true
 }
-
-func (ps *PathStack) ChangeDir(path string) {
-    if path == ".." {
-        ps.Pop()
-    } else {
-        ps.Push(path)
-    }
-}
-
-func (ps *PathStack) Push(path string) {
-    ps.paths = append(ps.paths, path)
-}
-
-func (ps *PathStack) CurrentPath() string {
-    length := len(ps.paths)
-
-    if length < 1 {
-        panic("Blew up in curentPath() something went wrong parsing")
-    }
-    val := ps.paths[length - 1]
-    return val
-}
-
-func (ps *PathStack) Pop() string {
-    length := len(ps.paths)
-
-    if length < 1 {
-        panic("Blew up in Pop(), something went wrong parsing")
-    }
-    val := ps.paths[length - 1]
-    ps.paths = ps.paths[0:length-1]
-
-    return val
-}
-
-type DirectoryTable map[string][]string
-
-func (dt *DirectoryTable) AddTo(dirName string, path string) {
-    if _, ok := (*dt)[path]; ok {
-        (*dt)[path] = append((*dt)[path], dirName) 
-    } else {
-        (*dt)[path] = []string{dirName}
-    }
-}
-
-type DirSize map[string]int
-
-func (ds *DirSize) AddTo(size int, path string) {
-    if current, ok := (*ds)[path]; ok {
-        (*ds)[path] = current + size 
-    } else {
-        (*ds)[path] = size 
-    }
-}
-
 
 
 func compute(input string) int {
+
     lines := strings.Split(input, "\n")
+    dirs := Set{"/": true}
+    files := map[string]int{}
+    pwd := "/"
 
-    dirTable := DirectoryTable{}
-    pathStack := PathStack{}
-    dirSizes := DirSize{}
-
-    for _, line := range lines {
+    for _, line := range lines[1:] {
         wds := strings.Split(line, " ")
 
-        if line[0] == '$' {
-            fmt.Println("command:", line)
-            if wds[1] == "cd" {
-                pathStack.ChangeDir(wds[2])
+        fmt.Println(line)
+
+        if strings.HasPrefix(line, "$ cd ..") {
+            if pwd == "/" {
             } else {
+                pwd = filepath.Dir(pwd)
             }
+        } else if strings.HasPrefix(line, "$ cd"){
+            pwd = filepath.Join(pwd, wds[2])
+        } else if strings.HasPrefix(line, "$ ls") {
+        } else if strings.HasPrefix(line, "dir") {
+            dirs.Add(filepath.Join(pwd, wds[1]))
+        } else if line == "" {
+            continue
         } else {
-            fmt.Println("output:", line)
-            if wds[0] == "dir" {
-                dirTable.AddTo(wds[1], pathStack.CurrentPath())
-            } else {
-                val, _ := strconv.Atoi(wds[0])
-                fmt.Println(val)
-                dirSizes.AddTo(val, pathStack.CurrentPath())
-            }
+            size, _ := strconv.Atoi(wds[0])
+            files[filepath.Join(pwd, wds[1])] = size
         }
     }
 
-    /*
-        map[/:23352670 a:94269 d:24933642 e:584]
-        map[/:[a d] a:[e]]
-    */
-
-    ret := 0
-
-    for dir, nestedDirs := range dirTable {
-        fmt.Println("Dir:", dir, "Contains dirs:", nestedDirs)
-        val, _ := dirSizes[dir]
-        tmp := val
-        for _, nestedDir := range nestedDirs {
-            tmp += dirSizes[nestedDir]
-        }
-
-        if tmp <= MAX_DIR_SIZE {
-            ret += tmp
-        }
+    root := 0
+    for _, v := range files {
+        root += v
+    }
+    if root > MAX_DIR_SIZE {
+        root = 0
     }
 
-    return ret
+    for dname, _ := range dirs {
+        root += Size(dname, files)
+    }
+
+    return root
 }
+
+
+func Size(dirname string, m map[string]int) int {
+    size := 0
+    for k, v := range m {
+        if strings.HasPrefix(k, dirname+"/") {
+            size += v
+        }
+    }
+    if size > MAX_DIR_SIZE {
+        return 0
+    } else {
+        return size
+    }
+}
+
 
 func main() {
     bytes, err := os.ReadFile("../input.txt")
